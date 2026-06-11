bab9af957a0874fec8ac96e9bc0a7989ca6af161
rdmmf <thomas.caillet@proton.me>
2026-06-11 21:09:31 +0200

    feat: collection breadcrumb redirects to /collections

--- bsimvis/app/static/js/breadcrumbs.js
+++ bsimvis/app/static/js/breadcrumbs.js
@@ -10,7 +10,7 @@ window.Breadcrumbs = {
         if (viewKey !== 'collections' && collection) {
             segments.push({
                 label: collection,
-                url: `/collections/${encodeURIComponent(collection)}/files`,
+                url: `/collections`,
                 icon: 'fa-solid fa-database'
             });
         }
